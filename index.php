988535ff9942d45c149bcbb599b4d2ab53254a9e
Richard <r.mahr@omnicom.de>
2014-02-19 09:42:48 +0100

    Add newsticker basic

--- index.php
+++ index.php
@@ -6,9 +6,19 @@
 <link rel="shortcut icon" type="image/x-icon" href="web/images/steen_32x32_schwarz.ico">
 <link rel="stylesheet" type="text/css" href="web/style.css">
 <title>steen</title>
-
+<script src="web/jquery-2.1.0.js" type="text/javascript"></script> 
+<script src="web/jquery.vticker.js" type="text/javascript"></script> 
 </head>
-<body>
+<body onload="$(function() {
+  $('#nextgig').vTicker('init', 
+		{
+			speed: 400, 
+			pause: 4000,
+			padding: 10,
+			showItems: 1
+		}
+	);
+});">
 <?php
 	include_once("web/pageControl.php");
 	
@@ -24,10 +34,21 @@
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1&appId=206800362749008";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+</script>
 
 	<div id="everything">
 		<div class="innerfull" id="header">
+		</div>
+		<div class="innerfull" id="nextgig">
+			<ul class="gigtickeritemscontainer innerfull">
+				<li class="gigtickeritem">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</li>
+				<li class="gigtickeritem">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</li>
+				<li class="gigtickeritem">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</li>
+			</ul>
 		</div>
 		<div class="innerfull" id="kachelcontainer">
 			<?php echo $pageControl->getKachelListHTML(); ?>
